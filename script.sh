17289779fafa411ccd0876d57858215791b89a6b
twoone3 <twoone3141@gmail.com>
2023-11-26 10:18:31 +0800

    Modified README.md and optimize code

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ MODDIR=${0%/*}
 port_yaml="$(cat "$MODDIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
 # 新建规则链
 iptables -t nat -N ADGUARDHOME
-iptables -t nat -A ADGUARDHOME ! -d "10.35.53.10" -p udp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
-iptables -t nat -A ADGUARDHOME ! -d "10.35.53.10" -p tcp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
+iptables -t nat -A ADGUARDHOME -p udp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
+iptables -t nat -A ADGUARDHOME -p tcp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
 iptables -t nat -I OUTPUT -j ADGUARDHOME
