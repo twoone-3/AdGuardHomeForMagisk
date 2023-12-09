5f0823b162ca1e5ffb2e748dc2a41c8f5b56fc35
twoone3 <twoone3141@gmail.com>
2023-12-09 23:32:55 +0800

    移除更新链接，修复新版适配问题，修改iptable规则，初步支持ipv6

--- service.sh
+++ service.sh
@@ -3,13 +3,15 @@ until [ $(getprop sys.boot_completed) ]; do
 done
 MODDIR=${0%/*}
 chmod +x "$MODDIR/AdGuardHome"
-"$MODDIR/AdGuardHome" >/dev/null 2>&1 &
+"$MODDIR/AdGuardHome" > "$MODDIR/AdGuardHome.log" 2>&1 &
 # 读取配置文件的端口
-port_yaml="$(cat "$MODDIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
+adhome_port="$(cat "$MODDIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
 # 新建规则链
 iptables -t nat -N ADHOME
 # 将 53 端口所有 upd tcp 流量转发到 adguard home
-iptables -t nat -A ADHOME -p udp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
-iptables -t nat -A ADHOME -p tcp --dport 53 -j DNAT --to-destination "127.0.0.1:$port_yaml"
+iptables -t nat -A ADHOME -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+iptables -t nat -A ADHOME -p tcp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+ip6tables -t nat -A ADHOME -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+ip6tables -t nat -A ADHOME -p tcp --dort 53 -j REDIRECT --to-ports ${adhome_port}
 # 将 ADHOME 规则添加到 OUTPUT
-iptables -t nat -I OUTPUT -j ADHOME
+iptables -t nat -A OUTPUT -j ADHOME
