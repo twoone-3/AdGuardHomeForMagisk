8c1571d3642c4ee5552523771c5deb8e6396f11b
twoone3 <twoone3141@gmail.com>
2023-12-18 19:42:02 +0800

    使用更优雅的代理逻辑，更换DNS，解决纯DNS回环问题，提高解析速度，更改AdGuardHome路径

--- README.md
+++ README.md
@@ -5,17 +5,18 @@
 ![GitHub all releases](https://img.shields.io/github/downloads/twoone-3/AdguardHome/total)
 
 # 这是什么？
-> 一个可以屏蔽广告的magisk模块，无需配置，在magisk里安装重启即可使用，使用前需关闭私人dns，不可用wap接入点，AdGuard Home后台管理地址http://127.0.0.1:3000，用户名/密码root
+> 一个通过过滤DNS请求来屏蔽广告的magisk模块，也能防止DNS污染，无需配置，在magisk里安装重启即可使用，使用前需关闭私人dns，AdGuard Home后台管理地址http://127.0.0.1:3000，用户名/密码root
 
 # Features
-- DNS上游为腾讯阿里的DOH（IP），稳定可靠
+- DNS上游为国内4大公共DNS，快速稳定
 - 仅内置[秋风广告规则](https://github.com/TG-Twilight/AWAvenue-Ads-Rule)，省电，不误杀
+- 
 
 # 特殊使用环境说明
-1. VPN类软件：SSR软件默认设置可用可过滤、V2RayNG软件默认设置可用可过滤，Clash软件默认设置加勾选网络-系统代理(可用但无法过滤)，其它软件自行测试。
+1. 代理软件：Clash Meta for Android 代理的应用无法过滤（可设置分应用来达到过滤效果），其它软件自行测试。
 2. 免流类Magisk模块：免流类模块在全局规则模式都可以使用。
 3. 翻墙类Magisk模块：AdGuard Home后台里的DNS上游需全部删除，加一条：127.0.0.1:X (X是翻墙模块的监听端口)，才可以使用。
-4. 模块默认监听端口为5591，若你有特殊需求，可在AdGuardHome.yaml文件修改 port: 5591 需要重启生效。(注意：若使用53端口，则无法开启热点)
+4. 模块默认监听端口为5591，若你有特殊需求，可在AdGuardHome.yaml文件修改 port: 5591 需要重启生效。
 
 # 鸣谢
 - [AdguardHome_magisk](https://github.com/410154425/AdGuardHome_magisk)
--- service.sh
+++ service.sh
@@ -1,17 +1,33 @@
 until [ $(getprop sys.boot_completed) ]; do
 	sleep 1
 done
-MODDIR=${0%/*}
-chmod +x "$MODDIR/AdGuardHome"
-"$MODDIR/AdGuardHome" > "$MODDIR/AdGuardHome.log" 2>&1 &
+MODDIR="${0%/*}"
+ADG_DIR="$MODDIR/bin"
+chmod +x "$ADG_DIR/AdGuardHome"
+chown "root:net_admin" "$ADG_DIR/AdGuardHome"
+chown "root:net_admin" "$ADG_DIR/AdGuardHome.yaml"
+setuidgid "root:net_admin" "$ADG_DIR/AdGuardHome" >"$ADG_DIR/AdGuardHome.log" 2>&1 &
 # 读取配置文件的端口
-adhome_port="$(cat "$MODDIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
-# 新建规则链
-iptables -t nat -N ADHOME
-# 将 53 端口所有 upd tcp 流量转发到 adguard home
-iptables -t nat -A ADHOME -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
-iptables -t nat -A ADHOME -p tcp --dport 53 -j REDIRECT --to-ports ${adhome_port}
-ip6tables -t nat -A ADHOME -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
-ip6tables -t nat -A ADHOME -p tcp --dort 53 -j REDIRECT --to-ports ${adhome_port}
-# 将 ADHOME 规则添加到 OUTPUT
-iptables -t nat -A OUTPUT -j ADHOME
+adhome_port="$(cat "$ADG_DIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
+
+apply_rules() {
+	# 新建规则链
+	iptables -t nat -N ADGUARD
+	# 返回所有 AdGuardHome 的请求
+	iptables -t nat -A ADGUARD -m owner --uid-owner "root" --gid-owner "net_admin" -j RETURN
+	# 将 53 端口所有 udp 流量转发到 adguard home
+	iptables -t nat -A ADGUARD -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+	ip6tables -t nat -A ADGUARD -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+	# 将 ADGUARD 规则添加到 OUTPUT
+	iptables -t nat -A OUTPUT -j ADGUARD
+}
+
+flush_rules() {
+	iptables -t mangle -F ADGUARD
+	iptables -t mangle -X ADGUARD
+
+	ip6tables -t mangle -F ADGUARD
+	ip6tables -t mangle -X ADGUARD
+}
+
+apply_rules
