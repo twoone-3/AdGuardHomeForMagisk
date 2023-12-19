13f2b44f777e89e7e8378615609f9a9cb34d6c17
twoone3 <twoone3141@gmail.com>
2023-12-19 10:50:32 +0800

    更新自述文件，添加回tcp

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 
 ![Static Badge](https://img.shields.io/badge/arm--64-support-blue)
 ![GitHub all releases](https://img.shields.io/github/downloads/twoone-3/AdguardHome/total)
+[![Telegram](https://img.shields.io/badge/Telegram-2CA5E0.svg?logo=telegram&logoColor=white)](https://t.me/adguardhome_for_magisk_release)
 
 # 这是什么？
 > 一个通过过滤DNS请求来屏蔽广告的magisk模块，也能防止DNS污染，无需配置，在magisk里安装重启即可使用，使用前需关闭私人dns，AdGuard Home后台管理地址http://127.0.0.1:3000，用户名/密码root
 
 # Features
-- DNS上游为国内4大公共DNS，快速稳定
+- DNS上游为DoH，快速稳定
 - 仅内置[秋风广告规则](https://github.com/TG-Twilight/AWAvenue-Ads-Rule)，省电，不误杀
-- 
 
 # 特殊使用环境说明
 1. 代理软件：Clash Meta for Android 代理的应用无法过滤（可设置分应用来达到过滤效果），其它软件自行测试。
--- service.sh
+++ service.sh
@@ -4,9 +4,10 @@ done
 MODDIR="${0%/*}"
 ADG_DIR="$MODDIR/bin"
 chmod +x "$ADG_DIR/AdGuardHome"
-chown "root:net_admin" "$ADG_DIR/AdGuardHome"
-chown "root:net_admin" "$ADG_DIR/AdGuardHome.yaml"
-setuidgid "root:net_admin" "$ADG_DIR/AdGuardHome" >"$ADG_DIR/AdGuardHome.log" 2>&1 &
+chown "root:adguard" "$ADG_DIR/AdGuardHome"
+chown "root:adguard" "$ADG_DIR/AdGuardHome.yaml"
+setuidgid "root:adguard" "$ADG_DIR/AdGuardHome" >"$ADG_DIR/AdGuardHome.log" 2>&1 &
+
 # 读取配置文件的端口
 adhome_port="$(cat "$ADG_DIR/AdGuardHome.yaml" | egrep '^  port: ' | sed -n 's/  port: //g;s/ //g;$p')"
 
@@ -14,10 +15,12 @@ apply_rules() {
 	# 新建规则链
 	iptables -t nat -N ADGUARD
 	# 返回所有 AdGuardHome 的请求
-	iptables -t nat -A ADGUARD -m owner --uid-owner "root" --gid-owner "net_admin" -j RETURN
-	# 将 53 端口所有 udp 流量转发到 adguard home
+	iptables -t nat -A ADGUARD -m owner --uid-owner "root" --gid-owner "adguard" -j RETURN
+	# 将 53 端口所有 udp tcp 流量转发到 adguard home
 	iptables -t nat -A ADGUARD -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+	iptables -t nat -A ADGUARD -p tcp --dport 53 -j REDIRECT --to-ports ${adhome_port}
 	ip6tables -t nat -A ADGUARD -p udp --dport 53 -j REDIRECT --to-ports ${adhome_port}
+	ip6tables -t nat -A ADGUARD -p tcp --dport 53 -j REDIRECT --to-ports ${adhome_port}
 	# 将 ADGUARD 规则添加到 OUTPUT
 	iptables -t nat -A OUTPUT -j ADGUARD
 }
