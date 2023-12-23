a36588a794d4132a9622a0e2e6b0e70aa7eefcaa
twoone3 <twoone3141@gmail.com>
2023-12-23 12:09:42 +0800

    修复LF问题，更新广告规则，解耦合iptables脚本以配合第三方软件实现启停，修改上游DNS为DoT

--- customize.sh
+++ customize.sh
@@ -1,12 +1,11 @@
 if [ "$ARCH" != "arm64" ]; then
-  ui_print "仅支持arm64架构，安装失败"
-  exit 1
+  abort "仅支持 arm64 架构，安装失败"
 fi
 
 ADG_DIR="$MODPATH/bin"
-chmod 777 "$ADG_DIR/AdGuardHome"
+chmod +x "$ADG_DIR/AdGuardHome" "$MODPATH/apply_iptables.sh" "$MODPATH/flush_iptables.sh"
 # TODO: 研究用户组对程序运行有什么影响
-chgrp net_raw "$ADG_DIR/AdGuardHome"
-chgrp net_raw "$ADG_DIR/AdGuardHome.yaml"
+# chgrp net_raw "$ADG_DIR/AdGuardHome"
+# chgrp net_raw "$ADG_DIR/AdGuardHome.yaml"
 
-ui_print "安装成功"
+ui_print "安装成功，请重启设备"
