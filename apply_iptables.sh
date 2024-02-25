5a782e9ba1f29e6946a0ff09fe0ad5e9ed73127b
twoone3 <twoone3141@gmail.com>
2024-02-25 11:40:03 +0800

    update bin and improve configs
    AdGuard Home v0.107.44
    configs schma 28

--- apply_iptables.sh
+++ apply_iptables.sh
@@ -1,6 +1,6 @@
 iptables -t nat -N ADGUARD
 # 返回所有 AdGuardHome 的请求
-iptables -t nat -A ADGUARD -m owner --uid-owner root --gid-owner net_raw -j RETURN
+iptables -t nat -A ADGUARD -m owner --uid-owner root --gid-owner net_admin -j RETURN
 # 将 53 端口所有 udp tcp 流量转发到 adguard home
 iptables -t nat -A ADGUARD -p udp --dport 53 -j REDIRECT --to-ports 5591
 iptables -t nat -A ADGUARD -p tcp --dport 53 -j REDIRECT --to-ports 5591
--- service.sh
+++ service.sh
@@ -1,9 +1,9 @@
 MODDIR="${0%/*}"
 ADG_DIR="$MODDIR/bin"
-# 输出重定向
+# redirect the log
 exec 1>"$ADG_DIR/stdout.log"
 exec 2>"$ADG_DIR/stderr.log"
 
-setuidgid root:net_raw "$ADG_DIR/AdGuardHome" &
+setuidgid root:net_admin "$ADG_DIR/AdGuardHome" &
 
 "$MODDIR/apply_iptables.sh"
